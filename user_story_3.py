8c7fc1c383a8441bfce7a610b8c8bb139c47b9c2
YXRSs <lzytoronto@gmail.com>
2017-10-23 01:25:09 -0400

    add the code and txt file for user story 3, but not yet complete

---
+++ user_story_3.py
@@ -0,0 +1,83 @@
+from tkinter import *
+root = Tk()
+
+def MCQ():
+    def write_questions():
+        nq = 'MCQ: ' + new_question.get('1.0',END)
+        fh = open('questions.txt', 'a')
+        fh.write(nq)
+        fh.close()
+        question_window.destroy()
+        
+    question_window = Toplevel(root)
+    question_window.geometry('600x300')
+    question_window.title('Multiple Choice window')   
+    
+    Label(question_window, text='Type your quetion here: ', font=20,pady=20).pack()
+    new_question = Text(question_window, width=40, height=10)
+    new_question.pack()
+    
+    confirm_button = Button(question_window,text='Confirm', command=write_questions)
+    exit_button = Button(question_window,text='Exit', command=question_window.destroy)
+    confirm_button.pack(side=LEFT,padx=150)
+    exit_button.pack(side=LEFT,padx=50)
+  
+def FBQ():
+    def write_questions():
+        nq = 'FBQ: ' + new_question.get('1.0',END)
+        fh = open('questions.txt', 'a')
+        fh.write(nq)
+        fh.close()
+        question_window.destroy()
+        
+    question_window = Toplevel(root)
+    question_window.geometry('600x300')
+    question_window.title('Fill in the blank window')   
+    
+    Label(question_window, text='Type your quetion here: ', font=20,pady=20).pack()
+    new_question = Text(question_window, width=40, height=10)
+    new_question.pack()
+    
+    confirm_button = Button(question_window,text='Confirm', command=write_questions)
+    exit_button = Button(question_window,text='Exit', command=question_window.destroy)
+    confirm_button.pack(side=LEFT,padx=150)
+    exit_button.pack(side=LEFT,padx=50)
+def MAQ():
+    def write_questions():
+        nq = 'MAQ: ' + new_question.get('1.0',END)
+        fh = open('questions.txt', 'a')
+        fh.write(nq)
+        fh.close()
+        question_window.destroy()
+        
+    question_window = Toplevel(root)
+    question_window.geometry('600x300')
+    question_window.title('Matching window')   
+    
+    Label(question_window, text='Type your quetion here: ', font=20,pady=20).pack()
+    new_question = Text(question_window, width=40, height=10)
+    new_question.pack()
+    
+    confirm_button = Button(question_window,text='Confirm', command=write_questions)
+    exit_button = Button(question_window,text='Exit', command=question_window.destroy)
+    confirm_button.pack(side=LEFT,padx=150)
+    exit_button.pack(side=LEFT,padx=50)
+
+
+
+top_Label = Label(root, text = 'Choose your question type here:')
+
+ 
+button1=Button(text="Multiple Choice", command=MCQ)
+button2=Button(text="Fill in the blanks", command=FBQ)
+button3=Button(text="Matching", command=MAQ)
+
+
+
+root.title("Create Questions")
+top_Label.pack()
+button1.pack(side = LEFT)
+button2.pack(side = LEFT)
+button3.pack(side = LEFT)
+
+root.mainloop()
