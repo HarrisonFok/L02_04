60a3b8d8edb5907899c68ca17ee365a6cd238665
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2017-11-02 16:24:04 -0400

    Function to make as many radio button you want.
    Can't program in Mobile?

---
+++ Automation/RadioButtons.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+v = IntVar()
+
+Label(root, text="""Choose a programming language:""", justify = LEFT, padx = 20).pack()
+
+Radiobutton(root, text="Python", padx = 20, variable=v, value=1).pack(anchor=W)
+
+Radiobutton(root, text="Perl", padx = 20, variable=v, value=2).pack(anchor=W)
+
+return(v)
