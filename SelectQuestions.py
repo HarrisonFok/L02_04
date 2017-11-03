381c0f21d1f19f73e80dc11e7f9206cebdd47e50
Harrison <pak.fok@mail.utoronto.ca>
2017-11-03 18:19:50 -0400

    Changed the method so that it uses .csv file. Incoroporated the randomization method but it did not work

--- SelectQuestions.py
+++ SelectQuestions.py
@@ -18,7 +18,7 @@ class SelectQuestions(tk.Frame):
         tk.Frame.__init__(self, *args, **kwargs)
 
         self.label = tk.Label(self, text="WELCOME!!\nPlease choose questions \
-to appear on an assignment\n", font=("Helvetica", 32))
+to appear on an assignment (without spaces)\n", font=("Helvetica", 32))
         self.label.pack()
 
         self.display_button = tk.Button(self, text="Display Questions", \
@@ -34,30 +34,27 @@ to appear on an assignment\n", font=("Helvetica", 32))
         
         # This will be the list of actual questions chosen
         self.chosenQuestions = []
+        
+        self.questionNumPair = {}
 
     def display(self):
-        # Display every question formula below
-        with open('questions.txt', 'r') as f:
-            # read the file and then split them by newline character
-            self.questions = f.read()
-            self.allQuestions = self.questions.split('\n')
-
-            # add every question to the GUI
-            for i in range(len(self.allQuestions)-1):
-
-                # display the question
-                self.singleQuestion = tk.Label(self, text=str(i) + ":" + \
-                                               self.allQuestions[i].\
-                                               split('|||')[0].split(':')[1],
+        # Read all the questions in questions.csv and display them on the
+        # screen
+        with open("questions.csv", "r") as csvFile:
+            reader = csv.reader(csvFile, delimiter="\n")
+            for row in reader:
+                splitRow = row[0].split(',')
+                self.questionNumPair[splitRow[0]] = splitRow[3]
+                self.singleQuestion = tk.Label(self, text=splitRow[0] + ":" + \
+                                               splitRow[3],\
                                                font=("Helvetica", 28))
-                
                 self.singleQuestion.pack()
-
-        self.submit_button.pack()
-
-        self.entry.pack()
-        
-        #self.display_button.config(state = 'disabled')
+                
+            self.submit_button.pack()
+    
+            self.entry.pack()
+            
+            self.display_button.config(state = 'disabled')
 
     def create_window(self):
         # Create a new window for the assignment
@@ -67,51 +64,21 @@ to appear on an assignment\n", font=("Helvetica", 32))
 
         # Get the list of question numbers the user chose and search in the 
         # text file
-        chosenQuestionNums = self.entry.get().split(',')
+        chosenQuestionNums = self.entry.get()
+        questionNumChosen = chosenQuestionNums.split(',')
         
         # Will call the validation method when the Submit button is pressed
-
-        # Read all the questions in questions.txt and split them into a list
-        with open('questions.txt', 'r') as f:
-            self.questions = f.read()
-            self.allQuestions = self.questions.split('\n')
-            
-            # write instruction
-            instructions = tk.Label(assignmentWindow, text="Please answer the \
-following questions to the best of your abilities\n\n", font=("Helvetica", 28))
-            instructions.pack()
-            
-            # If any chosen question numbers appear in the list, add the label
-            # to the new screen
-            for questionNum in range(len(self.allQuestions)-1):
-                if str(questionNum) in chosenQuestionNums:
-                    currentQuestion = self.allQuestions[questionNum]\
-                        .split('|||')[0].split(':')[1]
-                    
-                    # add the question to the list
-                    self.chosenQuestions.append(currentQuestion)
-                    
-                    # put the current chosen question on the screen
-                    self.question = tk.Label\
-                        (assignmentWindow,text=currentQuestion,\
-                         font=("Helvetica", 28))
-                    self.question.pack()
-                    
-                    # add an entry for the user to input
-                    self.assignmentEntry = tk.Entry(assignmentWindow)
-                    self.assignmentEntry.pack()
-                    
-                    # add a blank line to the GUI
-                    tk.Label(assignmentWindow, text="\n").pack()
-            assignmentWindow.geometry("%dx%d" % (2000, 2000))
-            
-            assignment_submit_button = tk.Button(assignmentWindow,\
-                                                 text = "Submit",\
-                                                 command=self.validate)
-            assignment_submit_button.pack()
-            
-            tk.Button(assignmentWindow, text="Exit",\
-                      command=assignmentWindow.destroy).pack()
+        
+        # Get the questions and store them in self.chosenQuestions
+        for key, value in self.questionNumPair.items():
+            for chosenNum in questionNumChosen:
+                if str(key).strip() == str(chosenNum).strip():
+                    # Add the question into self.chosenQuestions in the way
+                    # that makeAssignment requires
+                    self.chosenQuestions.append([self.questionNumPair.get(key)])
+        
+        # Make an assignment and store it inside Assignment.csv
+        makeAssignment(self.chosenQuestions)
 
     def RandomInRange(L):
         """
@@ -157,3 +124,10 @@ root.geometry('%sx%s' % (2000, 2000))
 main = SelectQuestions(root)
 main.pack(side="top", fill="both", expand=True)
 root.mainloop()
+
+'''
+self.question = tk.Label(assignmentWindow, text=\
+                         self.questionNumPair.get(key),\
+                         font=("Helvetica", 28))
+self.question.pack()
+'''
