50243798cf68b170033fd224c62abc0756acc1af
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2017-11-03 19:33:02 -0400

    Syntax works, haven't tested if it actually works...

--- Automation/RadioButtons.py
+++ Automation/RadioButtons.py
@@ -1,15 +1,11 @@
 from tkinter import *
 
-def Radios(FrameName, LabelText, ButtonTexts, Variable, DisplaySettings=None):
+def Radios(FrameName, LabelText, ButtonTexts, Variable=v, DisplaySettings=None):
 
   if DisplaySettings is None:
   
     Radio = [Label(FrameName, text=LabelText, justify = LEFT, padx = 20).pack()]
 
-    for t in ButtonTexts
+    for t in ButtonTexts:
 
-    Radiobutton(root, text="Python", padx = 20, variable=v, value=1).pack(anchor=W)
-
-    Radiobutton(root, text="Perl", padx = 20, variable=v, value=2).pack(anchor=W)
-
-    return(Variable)
+      Radio.append(Radiobutton(FrameName, text=t, padx = 20, variable=v, value=1).pack(anchor=W))
