0d1739764b6393753bf747e18205be650385071e
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2017-11-03 19:00:42 -0400

    Gotta test for Text emptiness and then iterate.

--- Automation/RadioButtons.py
+++ Automation/RadioButtons.py
@@ -1,13 +1,15 @@
 from tkinter import *
 
-root = Tk()
+def Radios(FrameName, LabelText, ButtonTexts, Variable, DisplaySettings=None):
 
-v = IntVar()
+  if DisplaySettings is None:
+  
+    Radio = [Label(FrameName, text=LabelText, justify = LEFT, padx = 20).pack()]
 
-Label(root, text="""Choose a programming language:""", justify = LEFT, padx = 20).pack()
+    for t in ButtonTexts
 
-Radiobutton(root, text="Python", padx = 20, variable=v, value=1).pack(anchor=W)
+    Radiobutton(root, text="Python", padx = 20, variable=v, value=1).pack(anchor=W)
 
-Radiobutton(root, text="Perl", padx = 20, variable=v, value=2).pack(anchor=W)
+    Radiobutton(root, text="Perl", padx = 20, variable=v, value=2).pack(anchor=W)
 
-return(v)
+    return(Variable)
