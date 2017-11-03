c26298d02689d731af38b0a67b20a90e77f4ec2f
williamjz <jizenan1996@hotmail.com>
2017-11-03 19:16:50 -0400

    a little modify

--- randomalgo.py
+++ randomalgo.py
@@ -13,7 +13,7 @@ def RandomInRange(L):
     //5
 
     """
-    return random.randint(L[0], L[1])
+    return random.randint(int(L[0]), int(L[1]))
 
 def makeAssignment(L):
     '''
@@ -22,19 +22,19 @@ def makeAssignment(L):
     '''
     result = []
     for question in L:
-        allrange = question[3].split(',')
+        allrange = question[4].split(',')
         vals = []
         for index in allrange:
             vals.append(RandomInRange(index.split('|')))
         i = 0
+        Q_B = question[3]
+        Q_A = question[5]
         for val in vals:
-            old = "VAR"+ str(i)
-            Q_B = question[2]
-            Q_A = question[4]
-            Q_B.replace(old, vals[i])
-            Q_A.replace(old, vals[i])
+            old = 'VAR'+ str(i)
+            Q_B = Q_B.replace(old, str(vals[i]))
+            Q_A = Q_A.replace(old, str(vals[i]))
             i += 1
-        result.append([question[1], Q_B, Q_A])
+        result.append([question[0], question[2], Q_B, Q_A])
 
     with open("Assignment.csv", "a") as csvfile:
         writer = csv.writer(csvfile)
@@ -42,4 +42,4 @@ def makeAssignment(L):
 
 
 if __name__ == '__main__':
-    makeAssignment()
+    makeAssignment([["25306175", "Topic_5", "MCQ", "What is VAR0 + VAR1", "12|15, 13|16", "VAR0+VAR1"],["25306175", "Topic_5", "MCQ", "What is VAR0 + VAR1", "12|15, 13|16", "VAR0+VAR1"] ])
