4992bad92d1485e34ae81231835b0992f4d7ed69
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2017-11-03 20:42:42 -0400

    This works, could add more options, but it works for now...

--- Automation/RadioButtons.py
+++ Automation/RadioButtons.py
@@ -1,11 +1,21 @@
 from tkinter import *
 
-def Radios(FrameName, LabelText, ButtonTexts, Variable=v, DisplaySettings=None):
+def Radios(FrameName, LabelText, ButtonTexts, v, DisplaySettings=None):
 
   if DisplaySettings is None:
-  
+
     Radio = [Label(FrameName, text=LabelText, justify = LEFT, padx = 20).pack()]
 
     for t in ButtonTexts:
 
       Radio.append(Radiobutton(FrameName, text=t, padx = 20, variable=v, value=1).pack(anchor=W))
+
+  return(Radio)
+
+root = Tk()
+
+v = IntVar()
+
+Radios(root, ["Hello"], ["Hello", "Goodbye", "Test", "ASD"], IntVar())
+
+mainloop()
