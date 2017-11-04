11b6ac2ed11cd53362de4c5a4c819c9630a6f3b7
Harrison <pak.fok@mail.utoronto.ca>
2017-11-04 17:40:42 -0400

    Able to call the randomization function and add to Assignment.csv

--- SelectQuestions.py
+++ SelectQuestions.py
@@ -13,6 +13,8 @@ import random
 import io
 import csv
 
+from randomalgo import *
+
 class SelectQuestions(tk.Frame):
 
     def __init__(self, *args, **kwargs):
@@ -71,62 +73,12 @@ to appear on an assignment (without spaces)\n", font=("Helvetica", 32))
         
         # Get the chosen questions and append to the list self.chosenQuestions
         with open("questions.csv", "r") as csvFile:
-            reader = csv.reader(csvFile, delimiter="\n")
-            for row in reader:
-                for num in questionNumChosen:
-                    if num.strip() in row[0]:
-                        self.chosenQuestions.append(row[0])
-
-        # For every element in self.chosenQuestions, split it by comma
-        for csvInfo in self.chosenQuestions:
-            indivList = csvInfo.split(',')
-            
-            # Add the element at index 5 with the one at index 4
-            indivList[4] = indivList[4] + ", " + indivList[5]
-            
-            # Delete the 5th element
-            del indivList[5]
-            
-            self.passList.append(indivList)
+            for lineList in csv.reader(csvFile):
+                self.chosenQuestions.append(lineList)
         
         # Make an assignment and store it inside Assignment.csv
-        self.makeAssignment(self.passList)
+        makeAssignment(self.chosenQuestions)
 
-    def RandomInRange(self, L):
-        """
-        len(L) == 2
-        list[int(min), int(max)] -> int
-        min <= max
-        a function that return a integer randomly in the range(min,max)
-        //RandomInRange(2, 6)
-        //5
-        """
-        return random.randint(L[0], L[1])
-    
-    def makeAssignment(self, L):
-        '''
-        :param L: the list of all the selected questions [[q1], [q2], [q3]]
-        '''
-        result = []
-        for question in L:
-            allrange = question[3].split(',')
-            vals = []
-            for index in allrange:
-                vals.append(self.RandomInRange(index.split('|')))
-            i = 0
-            for val in vals:
-                old = "VAR"+ str(i)
-                Q_B = question[2]
-                Q_A = question[4]
-                Q_B.replace(old, vals[i])
-                Q_A.replace(old, vals[i])
-                i += 1
-            result.append([question[1], Q_B, Q_A])
-    
-        with open("Assignment.csv", "a") as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(result)
-            
     def validate(self):
         # TODO later
         pass
@@ -143,3 +95,29 @@ self.question = tk.Label(assignmentWindow, text=\
                          font=("Helvetica", 28))
 self.question.pack()
 '''
+
+'''
+if row[0].strip() in questionNumChosen:
+    self.chosenQuestions.append(row[0])
+'''
+'''
+for row in reader:
+    for num in questionNumChosen:
+        if num.strip() in row[0]:
+            self.chosenQuestions.append(row[0])
+'''
+        #reader = csv.reader(csvFile, delimiter="\n")
+
+    # For every element in self.chosenQuestions, split it by comma
+    #for csvInfo in self.chosenQuestions:
+        #csvInfo.replace('"', '')
+        
+        #indivList = csvInfo.split(',')
+        
+        # Add the element at index 5 with the one at index 4
+        #indivList[4] = indivList[4] + ', ' + indivList[5]
+        
+        # Delete the 5th element
+        #del indivList[5]
+        
+        #self.passList.append(indivList)
