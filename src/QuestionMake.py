b285609bb7b9708f43e6b109a52d6e417dcb7085
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2017-11-05 10:53:35 -0500

    Name Change and Anti-Autorun.

--- src/QuestionMake.py
+++ src/QuestionMake.py
@@ -48,7 +48,7 @@ def MCQ():
     question_window = Toplevel(root)
     question_window.geometry('600x530')
     question_window.title('Multiple Choice window')
-    
+
     topic_L = Label(question_window, text='Related topic:', font=20,pady=20)
     topic_L.pack()
     topic_L.place(bordermode=OUTSIDE, x=10, y=0)
@@ -57,41 +57,41 @@ def MCQ():
     topic = OptionMenu(question_window, topic_num, "Topic_1", "Topic_2", "Topic_3", "Topic_4", "Topic_5")
     topic.pack()
     topic.place(bordermode=OUTSIDE, x=10, y=50)
-    
+
     Label(question_window, text='Type your question here: ', font=20,pady=20).pack()
     new_question = Text(question_window, width=40, height=10)
     new_question.pack()
-    
+
     var_button = Button(question_window,text="insert Variable",command=insert_var)
-    var_button.pack()    
-    
+    var_button.pack()
+
     Label(question_window, text='Type your answer formula here: ', font=20,pady=20).pack()
     answer_formula = Text(question_window, width=40, height=5)
     answer_formula.pack()
-    
+
     operator_button1 = Button(question_window,text="insert ' + '",command=insert_plus)
     operator_button1.pack()
     operator_button1.place(bordermode=OUTSIDE, x=50, y=320)
-    
+
     operator_button2 = Button(question_window,text="insert ' - '",command=insert_minus)
     operator_button2.pack()
     operator_button2.place(bordermode=OUTSIDE, x=50, y=370)
-    
+
     operator_button3 = Button(question_window,text="insert ' * '",command=insert_time)
     operator_button3.pack()
     operator_button3.place(bordermode=OUTSIDE, x=480, y=320)
-    
+
     operator_button4 = Button(question_window,text="insert ' / '",command=insert_divided)
     operator_button4.pack()
     operator_button4.place(bordermode=OUTSIDE, x=480, y=370)
-    
-    
+
+
     Label(question_window, text='', font=20,pady=20).pack()
     confirm_button = Button(question_window,text='Confirm', command=write_questions)
     exit_button = Button(question_window,text='Exit', command=question_window.destroy)
     confirm_button.pack(side=LEFT,padx=150)
     exit_button.pack(side=LEFT,padx=50)
-    
+
 def FBQ():
     li = [0]
 
@@ -118,12 +118,12 @@ def FBQ():
     def insert_divided():
         answer_formula.insert("insert", " / ")
     def get_topic():
-        return topic_num.get()    
-        
+        return topic_num.get()
+
     question_window = Toplevel(root)
     question_window.geometry('600x530')
-    question_window.title('Fill in the blank window')   
-    
+    question_window.title('Fill in the blank window')
+
     topic_L = Label(question_window, text='Related topic:', font=20,pady=20)
     topic_L.pack()
     topic_L.place(bordermode=OUTSIDE, x=10, y=0)
@@ -131,54 +131,51 @@ def FBQ():
     topic_num.set("choose here") # default value
     topic = OptionMenu(question_window, topic_num, "Topic_1", "Topic_2", "Topic_3", "Topic_4", "Topic_5")
     topic.pack()
-    topic.place(bordermode=OUTSIDE, x=10, y=50)    
-    
+    topic.place(bordermode=OUTSIDE, x=10, y=50)
+
     Label(question_window, text='Type your quetion here: ', font=20,pady=20).pack()
     new_question = Text(question_window, width=40, height=10)
     new_question.pack()
-    
+
     var_button = Button(question_window,text="insert Variable",command=insert_var)
-    var_button.pack()    
-    
+    var_button.pack()
+
     Label(question_window, text='Type your answer formula here: ', font=20,pady=20).pack()
     answer_formula = Text(question_window, width=40, height=5)
     answer_formula.pack()
-    
+
     operator_button1 = Button(question_window,text="insert ' + '",command=insert_plus)
     operator_button1.pack()
     operator_button1.place(bordermode=OUTSIDE, x=50, y=320)
-    
+
     operator_button2 = Button(question_window,text="insert ' - '",command=insert_minus)
     operator_button2.pack()
     operator_button2.place(bordermode=OUTSIDE, x=50, y=370)
-    
+
     operator_button3 = Button(question_window,text="insert ' * '",command=insert_time)
     operator_button3.pack()
     operator_button3.place(bordermode=OUTSIDE, x=480, y=320)
-    
+
     operator_button4 = Button(question_window,text="insert ' / '",command=insert_divided)
     operator_button4.pack()
-    operator_button4.place(bordermode=OUTSIDE, x=480, y=370)    
-    
+    operator_button4.place(bordermode=OUTSIDE, x=480, y=370)
+
     confirm_button = Button(question_window,text='Confirm', command=write_questions)
     exit_button = Button(question_window,text='Exit', command=question_window.destroy)
     confirm_button.pack(side=LEFT,padx=150)
     exit_button.pack(side=LEFT,padx=50)
 
 
+def QuestionMakeScreen():
 
+    top_Label = Label(root, text = 'Choose your question type here:')
 
-top_Label = Label(root, text = 'Choose your question type here:')
+    button1=Button(text="Multiple Choice", command=MCQ)
+    button2=Button(text="Fill in the blanks", command=FBQ)
 
- 
-button1=Button(text="Multiple Choice", command=MCQ)
-button2=Button(text="Fill in the blanks", command=FBQ)
+    root.title("Create Questions")
+    top_Label.pack()
+    button1.pack(side = LEFT)
+    button2.pack(side = LEFT)
 
-
-
-root.title("Create Questions")
-top_Label.pack()
-button1.pack(side = LEFT)
-button2.pack(side = LEFT)
-
-root.mainloop()
+    root.mainloop()
