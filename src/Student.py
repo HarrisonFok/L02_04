07dc8fdac25c2cd939c3373a653824d4c7293835
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2017-11-05 11:01:52 -0500

    Intregrating its index.py. Breaks 'cause User breaks.

--- src/Student.py
+++ src/Student.py
@@ -1,9 +1,41 @@
 from User import *
+
 class Student(User):
 	"""docstring for Student"""
-	def __init__(self, name, email, password, studentNumber):
+	def __init__(self, name="", email="", password="", studentNumber=""):
 		""" Constructor for Student object """
 		super().__init__(name, email, password, studentNumber, "S")
-	
-	def insertStudent(self):
-		super().insertUser()
+
+	def insertSelf(self):
+		super().insertSelf("Students.csv")
+
+	def signUpIndex(root):
+		""" Index page for the Profile Registration """
+
+		header = Label(root, text=(str(self.__class__)[17:-2] + "Profile Registration")).grid(row=0,column=1)
+
+		nameLabel = Label(root, text="Name:").grid(row=1)
+		nameEntry = Entry(root, width=50)
+		nameEntry.grid(row=1, column=1)
+
+		emailLabel = Label(root, text="Email:").grid(row=2)
+		emailEntry = Entry(root, width=50)
+		emailEntry.grid(row=2, column=1)
+
+		passwordLabel = Label(root, text="Password:").grid(row=3)
+		passwordEntry = Entry(root, show="*", width=50)
+		passwordEntry.grid(row=3, column=1)
+
+		passwordDupLabel = Label(root, text="Confirm Password:").grid(row=4)
+		passwordDupEntry = Entry(root, show="*", width=50)
+		passwordDupEntry.grid(row=4, column=1)
+
+		UserNumLabel = Label(root, text="Student Number:").grid(row=5)
+		UserNumEntry = Entry(root, width=50)
+		UserNumEntry.grid(row=5, column=1)
+
+		submitBtn = Button(root, text="Submit", command = lambda: self.createSelf(root, nameEntry, emailEntry, passwordEntry, passwordDupEntry, studentNumEntry))
+		submitBtn.grid(row=6, column=1)
+
+		# set fixed window size
+		root.resizable(width=False, height=False)
