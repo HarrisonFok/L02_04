b0b69d20b2648e31d8a6ed6e288eb8c4f5c4bea3
Harrison <pak.fok@mail.utoronto.ca>
2017-11-27 10:48:41 -0500

    Fixed the registration issue

--- src/Professor.py
+++ src/Professor.py
@@ -2,7 +2,7 @@ from User import *
 
 class Professor(User):
 	"""docstring for Professor"""
-	def __init__(self, _id, name, email, password, employeeNumber):
+	def __init__(self, name, email, password, employeeNumber, _id=None):
 		""" Constructor for Professor object """
 		super().__init__(name, email, password, employeeNumber, "P", _id)
 		self._courses = []
