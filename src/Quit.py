9593d526c8cf07f1458df29f6ac635ea4262b223
AlleyDismay <31709905+AlleyDismay@users.noreply.github.com>
2017-11-26 19:14:05 -0500

    Indentation Fix.

--- src/Quit.py
+++ src/Quit.py
@@ -1,8 +1,8 @@
 import SignIn
 
 def quit(window, user):
-	""" This method takes a window and current user and destroys the current window,
-	then redirects to the transition screen based on the current user. """
+    """ This method takes a window and current user and destroys the current window,
+    then redirects to the transition screen based on the current user. """
     # destroy this window
     window.destroy()
     # call the transition screen
